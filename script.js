de0111897b30facaba0aa4c53b2528b4af07eded
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-12-21 18:47:02 +0200

    feat: add validation check for password fields
    Added the functions validatePassword and validatePasswordMatch, to check
    whether the passwords entered conform to the set rules (are more than 6
    characters in length, contains at least one uppercase character, and
    contains at least one number), and that they match, respectively.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var inputFields = document.querySelectorAll('.input-flex input');
+  const inputFields = document.querySelectorAll('.input-flex input');
 
   inputFields.forEach(function(input) {
     input.addEventListener('blur', function() {
@@ -9,5 +9,45 @@ document.addEventListener('DOMContentLoaded', function() {
         input.classList.remove('visited');
       }
     });
+  });
+
+  const passwordInput = document.getElementById('password');
+  const confirmPasswordInput = document.getElementById('password-confirm');
+  const passwordInfo = document.querySelector('.password-info')
+  const spanMessage = document.querySelector('span.non-match');
+
+  function validatePasswordMatch() {
+    var isValid = passwordInput.value === confirmPasswordInput.value;
+
+    if (isValid) {
+      spanMessage.classList.remove('visible');
+    } else {
+      spanMessage.classList.add('visible');
+    }
+  }
+
+  function validatePassword(passwordField) {
+    const regex = /^(?=.*\d)(?=.*[A-Z]).{7,}$/;
+
+    const isPasswordValid = regex.test(passwordField.value);
+    const isPasswordEmpty = passwordField.value.trim() === "";
+
+    if (!isPasswordValid && !isPasswordEmpty) {
+      passwordField.classList.add('invalid');
+      passwordInfo.classList.add('invalid');
+    } else {
+      passwordField.classList.remove('invalid');
+      passwordInfo.classList.remove('invalid');
+    }
+  }
+
+  passwordInput.addEventListener('input', function() {
+    validatePassword(passwordInput);
+    validatePasswordMatch();
+  });
+
+  confirmPasswordInput.addEventListener('input', function() {
+    validatePassword(confirmPasswordInput);
+    validatePasswordMatch();
   });
 });
